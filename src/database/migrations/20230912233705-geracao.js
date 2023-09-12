b3798a72afe2d33524324935c8dd087ed6a4e83d
ThiagoThe <nitrox2thi@gmail.com>
2023-09-12 20:41:03 -0300

    feat: criado migration geracao

---
+++ src/database/migrations/20230912233705-geracao.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("geracao", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      dataGeracao: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      totalKw: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("geracao");
+  },
+};
