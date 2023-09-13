ef950893dbae631865c0eac6ad24bc74f560c790
ThiagoThe <nitrox2thi@gmail.com>
2023-09-13 19:21:34 -0300

    feat: add database config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,16 +1,17 @@
-const { config } = require('dotenv')
-config() 
+const { config } = require("dotenv");
+config();
 
-const { DIALECT, USERNAMEDB, PASSWORDDB, DATABASE, PORT, LOCALHOST } = process.env
+const { DIALECT, USERNAMEDB, PASSWORDDB, DATABASE, PORT, LOCALHOST } =
+  process.env;
 
 module.exports = {
-    dialect: DIALECT,
-    username: USERNAMEDB,
-    password: PASSWORDDB,
-    database: DATABASE,
-    port: PORT,
-    host: LOCALHOST,
-    define: {
-        underscoreAll: true
-    }
-}
+  dialect: DIALECT,
+  username: USERNAMEDB,
+  password: PASSWORDDB,
+  database: DATABASE,
+  port: PORT,
+  host: LOCALHOST,
+  define: {
+    underscoreAll: true,
+  },
+};
